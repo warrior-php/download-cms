789e2d1c7a33d3241659fc8ea8dd0f804ae4588a
Tony <57773117@qq.com>
2025-05-09 15:08:10 -0400

    新增Mailgun邮件

--- app/Controllers/Index.php
+++ app/Controllers/Index.php
@@ -3,19 +3,31 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Services\MailgunService;
+use Psr\Http\Client\ClientExceptionInterface;
 use support\Response;
 use Warrior\RateLimiter\Annotation\RateLimiter;
 
 class Index
 {
     /**
+     * 注入验证依赖
+     *
+     * @Inject
+     * @var MailgunService
+     */
+    protected MailgunService $mailgun;
+
+    /**
      * 网站首页
      *
      * @return Response
+     * @throws ClientExceptionInterface
      */
     #[RateLimiter(limit: 3, ttl: 1)]
     public function index(): Response
     {
+        $this->mailgun->send('weplus.cc@gmail.com', 'Test Email', '这是一封测试邮件');
         return view('index');
     }
 }
